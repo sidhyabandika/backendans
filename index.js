c85d448ae93f5a43cef27c95855602f384d56cab
Abangdika <dikabejong06@gmail.com>
2021-10-15 17:53:42 +0800

    Procfile update

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require ("express");
+const app = express();
+const importData = require("./db.json");
+let port = process.env.PORT;
+
+app.get ("/",(req, res) =>{
+    res.send("Selamat Datang di Access Network Society");
+});
+app.get("/car",(req, res) =>{
+    res.send(importData);
+
+
+});
+app.listen(port,() => {
+    console.log('example app is listening on port');
+});
+
+
