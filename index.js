fcc0f1920dd270ecb842da5ffb2bb22af66515ba
Abangdika <dikabejong06@gmail.com>
2021-10-24 02:47:55 +0800

    update

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require ("express");
 const app = express();
 const importData = require("./db.json");
 const importData1 = require("./dbadat.json");
-const importData2 = require("./moderncasual.json");
-
+const importData2 = require("./moderncasualpria.json");
+const importData3 = require("./moderncasualwanita.json");
+const importData4 = require("./fashionpantaipria.json");
+const importData5 = require("./fashionpantaiwanita.json");
+const importData6 = require("./old.json");
 let port = process.env.PORT;
 
 app.get ("/",(req, res) =>{
@@ -15,8 +18,20 @@ app.get("/car",(req, res) =>{
 app.get("/adat",(req, res) =>{
     res.send(importData1);
 });
-app.get("/modern/casual",(req, res) =>{
+app.get("/modern/casual/pria",(req, res) =>{
     res.send(importData2);
+});
+app.get("/modern/casual/wanita",(req, res) =>{
+    res.send(importData3);
+});
+app.get("/pantai/pria",(req, res) =>{
+    res.send(importData4);
+});
+app.get("/pantai/wanita",(req, res) =>{
+    res.send(importData5);
+});
+app.get("/old",(req, res) =>{
+    res.send(importData6);
 });
 
 app.listen(port,() => {
